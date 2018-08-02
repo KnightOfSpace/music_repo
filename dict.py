0b3e2c03b62f1e9cab9647178229761be5bd50cf
Brianna Sanchez Ortega <briannaxsanchez@gmail.com>
2018-08-02 21:40:02 +0000

    Handled Merge Conflict

--- dict.py
+++ dict.py
@@ -1,12 +1,11 @@
 def print_dict(dict):
     for key in dict:
-<<<<<<< HEAD
+
         print dict[key]
-=======
-        print key
+
         
 song = {
     "Artist":"Drake",
     "song_name":"God's plan"
 }
->>>>>>> 4f092d34e8be5df20c6cde42c482af7c5a0b838d
+
